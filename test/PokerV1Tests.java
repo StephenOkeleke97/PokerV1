ccfb56cfc70738f27fb6360705ab2c69fc5def01
Stephen Okeleke <stephenokeleke@yahoo.com>
2022-04-15 19:48:44 -0700

    test plan basic methods

---
+++ test/PokerV1Tests.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PokerV1Tests {
+
+  @BeforeEach
+  void setUp() throws Exception {
+  }
+  
+  @Test
+  public void testGetRankOfAce() {
+
+  }
+
+  @Test
+  public void testGetRankOfKing() {
+
+  }
+
+  @Test
+  public void testGetRankOfQueen() {
+
+  }
+
+  @Test
+  public void testGetRankOfJack() {
+
+  }
+
+  @Test
+  public void testGetRankOfNumberedSuite() {
+
+  }
+
+}
