5282b6b9c340310d3f599180df4a201f1ae92417
Stephen Okeleke <stephenokeleke@yahoo.com>
2022-04-20 19:16:26 -0700

    Completed test plan

--- test/PokerV1Tests.java
+++ test/PokerV1Tests.java
@@ -33,5 +33,153 @@ class PokerV1Tests {
   public void testGetRankOfNumberedSuite() {
 
   }
+  
+  public void testDeckContains52Cards() {
+
+  }
+
+  public void testDeckIsUnique() {
+
+  }
+
+  public void testSortCardsByNumber() {
+
+  }
+
+  public void testSortCardsByPower() {
+
+  }
+
+  public void testCreateHandWithMoreThan5CardsThrowsException() {
+
+  }
+
+  public void testCreateFlushWithMoreThan5CardsIsAllowed() {
+
+  }
+
+  public void testRoyalFlushEvaluatedCorrectly() {
+
+  }
+
+  public void testStraightFlushEvaluatedCorrectly() {
+
+  }
+
+  public void testFourOfAKindEvaluatedCorrectly() {
+
+  }
+
+  public void testFullHouseEvaluatedCorrectly() {
+
+  }
+
+  public void testFlushEvaluatedCorrectly() {
+
+  }
+
+  public void testStraightEvaluatedCorrectly() {
+
+  }
+
+  public void testThreeofAKindEvaluatedCorrectly() {
+
+  }
+
+  public void testTwoPairEvaluatedCorrectly() {
+
+  }
+
+  public void testPairEvaluatedCorrectly() {
+
+  }
+
+  public void testHighCardEvaluatedCorrectly() {
+
+  }
+
+  public void testCardsRankedCorrectlyDifferentHands() {
+
+  }
+
+  public void testAllHighCardsRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllHighCards() {
+
+  }
+  
+  public void testAllPairsRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllPairs() {
+
+  }
+  
+  public void testAllTwoPairsRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllTwoPairs() {
+
+  }
+  
+  public void testAllThreeOfAKindRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllThreeOfAKind() {
+
+  }
+  
+  public void testAllStraightsRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllStraights() {
+
+  }
+  
+  public void testAllFlushRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllFlush() {
+
+  }
+  
+  public void testAllFullHousesRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllFullHouses() {
+
+  }
+  
+  public void testAllFourOfAKindsRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllFourOfAKinds() {
+
+  }
+  
+  public void testAllStraightFlushRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllStraightFlush() {
+
+  }
+  
+  public void testAllRoyalFlushRankedCorrectly() {
+
+  }
+
+  public void testGetWinnerMultipleAllRoyalFlush() {
+
+  }
 
 }
